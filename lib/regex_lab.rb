d4e69c2d2e6a5600828a423729946c52dd976789
Ryna Truong <rynatruong@outlook.com>
2020-07-17 20:52:07 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
-end
+  if word.match(/^[aeiouAEIOU]/)
+    TRUE
+    else
+      false
+    end
+  end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\b[A-Z]\w+[.?!,]/)
+    true
+  else 
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\d{10}|\S\d{3}\S\d{7}|\d{3}\s\d{3}\s\d{4}|\D\d{3}\D\d{3}\D\d{4}/)
+    true
+  else
+    false
+  end
 end
